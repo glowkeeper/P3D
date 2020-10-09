b1719f2520163fd4d0d4e943b1338e1311af27f7
drhuckle <glowkeeper@yahoo.co.uk>
2020-10-09 10:21:32 +0100

    Week 2 Session 2

--- src/c++/main.cpp
+++ src/c++/main.cpp
@@ -7,11 +7,19 @@
 
 #include <iostream>
 
-color ray_color(const ray& r, const hittable& world) {
+color ray_color(const ray& r, const hittable& world, int depth) {
     hit_record rec;
-    if (world.hit(r, 0, infinity, rec)) {
-        return 0.5 * (rec.normal + color(1,1,1));
+
+    // If we've exceeded the ray bounce limit, no more light is gathered.
+    if (depth <= 0)
+        return color(0,0,0);
+
+    // Avoid Shadow Acne by ignoring hits very near zero
+    if (world.hit(r, 0.001, infinity, rec)) {
+        point3 target = rec.p + rec.normal + random_unit_vector();
+        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
     }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
@@ -24,7 +32,8 @@ int main() {
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 100;
+    const int samples_per_pixel = 100;    
+    const int max_depth = 50;
 
     // World
     hittable_list world;
@@ -44,8 +53,8 @@ int main() {
             for (int s = 0; s < samples_per_pixel; ++s) {
                 auto u = (i + random_double()) / (image_width-1);
                 auto v = (j + random_double()) / (image_height-1);
-                ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world);
+                ray r = cam.get_ray(u, v);                
+                pixel_color += ray_color(r, world, max_depth);
             }
             write_color(std::cout, pixel_color, samples_per_pixel);
         }
--- src/c++/vec3.h
+++ src/c++/vec3.h
@@ -45,6 +45,14 @@ class vec3 {
             return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
         }
 
+        inline static vec3 random() {
+            return vec3(random_double(), random_double(), random_double());
+        }
+
+        inline static vec3 random(double min, double max) {
+            return vec3(random_double(min,max), random_double(min,max), random_double(min,max));
+        }
+
     public:
         double e[3];
 };
@@ -56,47 +64,62 @@ using color = vec3;    // RGB color
 // vec3 Utility Functions
 
 inline std::ostream& operator<<(std::ostream &out, const vec3 &v) {
-return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
 }
 
 inline vec3 operator+(const vec3 &u, const vec3 &v) {
-return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
+    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]);
 }
 
 inline vec3 operator-(const vec3 &u, const vec3 &v) {
-return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
+    return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]);
 }
 
 inline vec3 operator*(const vec3 &u, const vec3 &v) {
-return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]);
 }
 
 inline vec3 operator*(double t, const vec3 &v) {
-return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
+    return vec3(t*v.e[0], t*v.e[1], t*v.e[2]);
 }
 
 inline vec3 operator*(const vec3 &v, double t) {
-return t * v;
+    return t * v;
 }
 
 inline vec3 operator/(vec3 v, double t) {
-return (1/t) * v;
+    return (1/t) * v;
 }
 
 inline double dot(const vec3 &u, const vec3 &v) {
-return u.e[0] * v.e[0]
-     + u.e[1] * v.e[1]
-     + u.e[2] * v.e[2];
+    return u.e[0] * v.e[0]
+         + u.e[1] * v.e[1]
+         + u.e[2] * v.e[2];
 }
 
 inline vec3 cross(const vec3 &u, const vec3 &v) {
-return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
             u.e[2] * v.e[0] - u.e[0] * v.e[2],
             u.e[0] * v.e[1] - u.e[1] * v.e[0]);
 }
 
 inline vec3 unit_vector(vec3 v) {
-return v / v.length();
+    return v / v.length();
+}
+
+vec3 random_in_unit_sphere() {
+    while (true) {
+        auto p = vec3::random(-1,1);
+        if (p.length_squared() >= 1) continue;
+        return p;
+    }
+}
+
+vec3 random_unit_vector() {
+    auto a = random_double(0, 2*pi);
+    auto z = random_double(-1, 1);
+    auto r = sqrt(1 - z*z);
+    return vec3(r*cos(a), r*sin(a), z);
 }
 
 #endif
